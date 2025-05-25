484a34e8976eb8df79b126155e6bc7f867c0d4ed
karthik <chkarthik853@gmail.com>
2025-05-25 23:35:10 +0530

    commit for practise

---
+++ shortsem_1/src/shortsem_1/practise.java
@@ -0,0 +1,15 @@
+package shortsem_1;
+
+import java.util.Scanner;
+
+public class practise {
+	public static void main(String[] args) {
+		Scanner x=new Scanner(System.in);
+		String a=x.next();
+		String b=x.next();
+		if(a.equals(b))
+			System.out.println("yes equal");
+		else
+			System.out.println("not equal");
+	}
+}
