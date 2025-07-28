41eca0bcc3ce397b04b11a0933373341a8d6a5b6
karthik <chkarthik853@gmail.com>
2025-07-28 09:40:29 +0530

    commit

--- leetcode/src/leetcode/correct.java
+++ leetcode/src/leetcode/correct.java
@@ -2,21 +2,22 @@ package leetcode;
 
 public class correct {
 	public static void main(String[] args) {
-		String a="leeetcccode";
-		char prev=a.charAt(0);
-		String b=String.valueOf(prev);
-		
-		int count=0;
-		for(int i=1;i<a.length();i++) {
-			char current=a.charAt(i);
-			if(prev==current) {
-				count++;
-				System.out.println("count is:"+count);
-				if(count<2)b+=a.charAt(i);
-			}else {
-				if(count<3)b+=a.charAt(i);
-				count=0;
-			}prev=current;
-		}System.out.println("string is :"+b);
+		String a="leetcode";
+		StringBuilder b = new StringBuilder();
+		if (a == null || a.isEmpty()) {
+		    System.out.println("string is :" + a);
+		    return;
+		}
+		b.append(a.charAt(0));
+
+		for (int i = 1; i < a.length(); i++) {
+		    char current = a.charAt(i);
+		    int bLength = b.length();
+		    if (bLength >= 2 && b.charAt(bLength - 1) == current && b.charAt(bLength - 2) == current) {
+		        continue;
+		    }
+		    b.append(current);
+		}
+		System.out.println("string is :" + b.toString());
 	}
 }
