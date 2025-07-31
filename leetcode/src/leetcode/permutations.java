64b5ffa1f3fcf81a2cdfbea3cc683ead5d6d20ea
karthik <chkarthik853@gmail.com>
2025-07-31 12:44:21 +0530

    commit

--- leetcode/src/leetcode/permutations.java
+++ leetcode/src/leetcode/permutations.java
@@ -1,20 +1,28 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class permutations {
 	public static void main(String[] args) {
 		String a="car";
-		genpermutations(a.toCharArray(),0);
+		List<String> l1=new ArrayList<>();
+		l1=genpermutations(a.toCharArray(),0);
+		System.out.println(Arrays.toString(l1.toArray(new String [0])));
 	}
-	private static void genpermutations(char[] charArray, int index) {
+	private static List<String> genpermutations(char[] charArray, int index) {
+		List<String> l1=new ArrayList<>();
 		if(index==charArray.length-1) {
-			System.out.println(String.valueOf(charArray));
-			return;
+			l1.add(String.valueOf(charArray));
+			return l1;
 		}
 		for(int i=index;i<charArray.length;i++) {
 			swap(charArray,index,i);
-			genpermutations(charArray,index+1);
+			List<String> perms=genpermutations(charArray,index+1);
+			l1.addAll(perms);
 			swap(charArray,index,i);
-		}
+		}return l1;
 		
 	}
 	private static void swap(char[] charArray, int index, int i) {
