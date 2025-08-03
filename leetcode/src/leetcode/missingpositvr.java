bae20ba5aa581cdaa2118bbcad320fd0a069a6e6
karthik <chkarthik853@gmail.com>
2025-08-03 15:29:18 +0530

    commit

--- leetcode/src/leetcode/missingpositvr.java
+++ leetcode/src/leetcode/missingpositvr.java
@@ -1,7 +1,31 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class missingpositvr {
 	public static void main(String[] args) {
-		System.out.println("hi");
+		int a[]= {1,2,3,5};
+		if (a.length == 0) {
+            System.out.println(1);
+            return;
+        }
+		Arrays.sort(a);
+		int f = 0;
+        while (f < a.length && a[f] <= 0) {
+            f++;
+        }
+		if(f >= a.length || a[f] != 1) {
+			System.out.println(1);
+			return;
+		}
+		for(int i=1;i<a.length;i++) {
+			if (a[i] == a[i-1]) {
+                continue; 
+            }
+			if(a[i-1]+1!=a[i] && a[i-1]>0) {
+				System.out.println(a[i-1]+1);
+				break;
+			}
+		}
 	}
 }
