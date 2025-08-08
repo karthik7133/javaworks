d1c5bd8a84b5fb207c17f1fc6ebc6db4538e67b3
karthik <chkarthik853@gmail.com>
2025-08-08 11:58:41 +0530

    commit

--- sts/src/classassignment/Euler_phi.java
+++ sts/src/classassignment/Euler_phi.java
@@ -1,31 +1,21 @@
 package classassignment;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Euler_phi {
 	public static void main(String[] args) {
-		int a=10;int temp=a;
-		List<Integer> l=new ArrayList<Integer>();
+		int a=500;double temp=a;
 		if(a%2==0) {
-			l.add(2);
-			while(a%2==0) {
-				a/=2;
-			}
+			while(a%2==0) a/=2;
+			temp=temp*(1.0-(1.0/2.0));
 		}
-		for(int i=3;i<Math.sqrt(temp);i+=2) {
+		for(int i=3;i*i<=a;i+=2) {
 			if(a%i==0) {
-				l.add(i);
 				while(a%i==0) {
 					a/=i;
-				}
+				}temp=temp*(1.0-(1.0/i));
 			}
-		}if(a>2)l.add(a);
-		System.out.println(Arrays.toString(l.toArray()));
-		for(int j:l) {
-			temp*=(1.0-(1.0/j));
-		}
-		System.out.println(temp);
+		}if(a>2) 
+			temp=temp*(1.0-(1.0/a));
+
+		System.out.println((int)temp);
 	}
 }
