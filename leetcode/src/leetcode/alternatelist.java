af3f4debc86665b50862fbac5ebb94932bc4cbb9
karthik <chkarthik853@gmail.com>
2025-08-08 14:24:47 +0530

    commit

--- leetcode/src/leetcode/alternatelist.java
+++ leetcode/src/leetcode/alternatelist.java
@@ -3,7 +3,9 @@ package leetcode;
 public class alternatelist {
 	public static void main(String[] args) {
 		sol a =new sol();
-		a.add(1);a.display();
+		a.add(1);
+		a.add(2);a.add(3);a.add(4);a.alternate();
+		a.display();
 	}
 }
 class sol{
@@ -32,4 +34,29 @@ class sol{
 			temp=temp.next;
 		}System.out.println("null");
 	}
+	public void alternate() {
+	    if (head == null || head.next == null) {
+	        return;
+	    }
+	    ListNode dummy = new ListNode(0);
+	    dummy.next = head;
+	    ListNode prev = dummy;
+	    ListNode first = head;
+	    ListNode second = head.next;
+	    while (first != null && second != null) {
+	        ListNode nextPair = second.next;
+	        second.next = first;
+	        prev.next = second;
+	        first.next = nextPair;
+	        prev = first;
+	        first = nextPair;
+	        if (first != null) {
+	            second = first.next;
+	        } else {
+	            second = null;
+	        }
+	    }
+	    this.head = dummy.next;
+	}
+
 }
