fa92286b21bdf814dd0f942d6dbd7ab4efcedad8
karthik <chkarthik853@gmail.com>
2025-08-10 11:35:36 +0530

    commit

--- leetcode/src/leetcode/bin_decimalin_linkedlist.java
+++ leetcode/src/leetcode/bin_decimalin_linkedlist.java
@@ -4,9 +4,10 @@ public class bin_decimalin_linkedlist {
 	public static void main(String[] args) {
 		sol2 s=new sol2();
 		s.add(1);
-		s.add(2);
-		s.add(3);
+		s.add(0);
+		s.add(1);
 		s.display();
+		s.num();s.convet();
 	}
 }
 class sol2{
@@ -43,5 +44,25 @@ class sol2{
 			sum+=(a%10)*Math.pow(2, count);count++;
 			a/=10;
 		}System.out.println(sum);
+	}
+	public String num() {
+		String a="";
+		if(head==null) {
+			System.out.println("List is empty");
+			return null; 
+		}
+		ListNode temp=head;
+		while(temp!=null) {
+			a+=String.valueOf(temp.val);
+			temp=temp.next;
+		}return a;
+	}
+	public void convet() {
+		ListNode temp=head;
+		int val=0;
+		while(temp!=null) {
+			val=val*2+temp.val;
+			temp=temp.next;
+		}System.out.println(val);
 	}
 }
