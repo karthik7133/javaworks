41a1077d701c31461d1d1799f74fed44a4aed8f0
karthik <chkarthik853@gmail.com>
2025-08-11 17:09:44 +0530

    commit

--- leetcode/src/leetcode/PLUSone.java
+++ leetcode/src/leetcode/PLUSone.java
@@ -2,18 +2,19 @@ package leetcode;
 
 import java.util.Arrays;
 
-public class PLUSone {
-	public static void main(String[] args) {
-		int a[]= {1,2,3};
-		String sum="";
-		for(int i=0;i<a.length;i++) {
-			sum+=a[i];
-		}
-		sum=String.valueOf(Integer.valueOf(sum)+1);
-		int b[]=new int[sum.length()];
-		for(int i=0;i<sum.length();i++) {
-			b[i]=Integer.valueOf(String.valueOf(sum.charAt(i)));
-		}
-		System.out.println(Arrays.toString(b));
-	}
+class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {9, 9, 9};
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                System.out.println(Arrays.toString(digits));
+                return;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        System.out.println(Arrays.toString(newDigits));
+    }
 }
