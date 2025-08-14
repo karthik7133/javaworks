d8e24285ff06c8646459d91dffa1eaca179b1916
karthik <chkarthik853@gmail.com>
2025-08-14 11:40:54 +0530

    commit

--- leetcode/src/leetcode/big3digitnum.java
+++ leetcode/src/leetcode/big3digitnum.java
@@ -6,21 +6,16 @@ import java.util.Collections;
 
 public class big3digitnum {
 	public static void main(String[] args) {
-        String num = "67771333399999";
-        ArrayList<Integer> threeDigitNumbers = new ArrayList<>();
-        
-        for (int i = 0; i <= num.length() - 3; i++) {
-            char current = num.charAt(i);
-            if (current == num.charAt(i + 1) && current == num.charAt(i + 2)) {
-                threeDigitNumbers.add(Character.getNumericValue(current));
+		String num="234444";
+		String result = "";
+        for (int i = 0; i < num.length() - 2; i++) {
+            if (num.charAt(i) == num.charAt(i + 1) && num.charAt(i + 1) == num.charAt(i + 2)) {
+                String current = num.substring(i, i + 3);
+                if (current.compareTo(result) > 0) {
+                    result = current;
+                }
             }
         }
-        
-        if (!threeDigitNumbers.isEmpty()) {
-            Collections.sort(threeDigitNumbers, Collections.reverseOrder());
-            System.out.print("\""+threeDigitNumbers.get(0)+"\"");
-        } else {
-            System.out.println("No three consecutive digits found.");
-        }
+        System.out.println(result) ;
     }
 }
