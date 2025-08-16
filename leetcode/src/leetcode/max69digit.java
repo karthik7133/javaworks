7aff3d60d36de12633f4d1eb2a9dd9d1a653dd28
karthik <chkarthik853@gmail.com>
2025-08-16 14:01:34 +0530

    commit

--- leetcode/src/leetcode/max69digit.java
+++ leetcode/src/leetcode/max69digit.java
@@ -4,9 +4,9 @@ public class max69digit {
 	public static void main(String[] args) {
 		int  n=6969;
 		char c[]=String.valueOf(n).toCharArray();
-		for(char ch:c) {
-			if(ch=='6') {
-				ch='9';break;
+		for( int i=0 ;i<c.length;i++) {
+			if(c[i]=='6') {
+				c[i]='9';break;
 			}
 		}
 		String k=String.valueOf(c);
