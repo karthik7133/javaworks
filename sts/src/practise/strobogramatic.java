22508b35876f4794b55a8b1abdcd3566f3012b03
karthik <chkarthik853@gmail.com>
2025-08-19 08:18:37 +0530

    commit

--- sts/src/practise/strobogramatic.java
+++ sts/src/practise/strobogramatic.java
@@ -4,21 +4,19 @@ public class strobogramatic {
 	public static void main(String[] args) {
 		Scanner x=new Scanner(System.in);
 		System.out.println("enter the number to check:");
-		HashMap<Character,Character> map=new HashMap<>();
-		map.put('0','0');
-		map.put('8','8');
-		map.put('9','6');
-		map.put('6','9');
-		
-		
-		String n=x.nextLine();
-		for(int i=0;i<(n.length()/2);i++) {
-			char c=n.charAt(i);char m=n.charAt(n.length()-i-1);
+		HashMap<Character ,Character> map=new HashMap<>();
+		map.put('0', '0');
+		map.put('1', '1');
+		map.put('9', '6');
+		map.put('6', '9');
+		map.put('8', '8');
+		String k=x.next();
+		for(int i=0;i<k.length()/2;i++) {
+			char c=k.charAt(i);char m=k.charAt(k.length()-i-1);
 			Character mirror=map.get(c);
 			if(mirror==null | mirror!=m) {
-				System.out.println("False");return;
+				System.out.println("Not stobrogramatic !");return ;
 			}
-			
-		}System.out.println("true");
+		}System.out.println("Yes it's stobrogramatic!");
 	}
 }
