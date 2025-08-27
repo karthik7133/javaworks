1e72196466ef564ada17e275e3696a394aef5409
karthik <chkarthik853@gmail.com>
2025-08-27 09:14:32 +0530

    commit

--- leetcode/src/leetcode/search_35.java
+++ leetcode/src/leetcode/search_35.java
@@ -3,26 +3,24 @@ package leetcode;
 public class search_35 {
 	public static void main(String[] args) {
 		int a[]={1,3,5,6};
-		System.out.println(searchInsert(a,7));
+		System.out.println(searchInsert(a,2));
 	}
 	public static int searchInsert(int[] nums, int target) {
-	        int st=0;int end=nums.length;
-	        int m=0;int mid=0;
-	        while(st<end) {
-	        	mid=st+(end-st)/2;
-	        	if(nums[mid]==target) {
-	        		System.out.println("Element found!");
-	        		return mid;
-	        	}
-	        	if(nums[mid]<target) {
-	        		st=mid+1;m=mid;
-	        	}
-	        	else {
-	        		end=mid-1;m=mid;
-	        	}
+	    int st = 0;
+	    int end = nums.length - 1;
+
+	    while (st <= end) {
+	        int mid = st + (end - st) / 2;
+
+	        if (nums[mid] == target) {
+	            return mid; 
 	        }
-	        if(m>=mid)
-	        return m+1;
-	        else return m-1;
+	        if (nums[mid] < target) {
+	            st = mid + 1; 
+	        } else {
+	            end = mid - 1; 
+	        }
+	    }
+	    return st;
 	}
 }
