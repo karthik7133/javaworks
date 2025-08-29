bc314ddd64ef81f30bd732fedd759d4d7c5892d2
karthik <chkarthik853@gmail.com>
2025-08-29 09:44:32 +0530

    commit

---
+++ sts/src/cat_2/longcon_1.java
@@ -0,0 +1,19 @@
+package cat_2;
+import java.util.*;
+public class longcon_1 {
+	public static void main(String[] args) {
+		Scanner x= new Scanner(System.in);
+		System.out.println("Enter the Binary String:");
+		String k=x.nextLine();
+		int count=0,max=0,p=0;
+		for(int i=0;i<k.length();i++) {
+			if(k.charAt(i)=='1')count++;
+			else {
+				max=Math.max(max, count+p+1);
+				p=count;
+				count=0;
+			}
+		}max=Math.max(max, count+p+1);
+		System.out.println(max);
+	}
+}
