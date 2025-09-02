5bdd323b6736852bc76baa0f872bcafc43400ebf
karthik <chkarthik853@gmail.com>
2025-09-02 10:13:49 +0530

    commit

---
+++ sts/src/cat_2/swap_nibble.java
@@ -0,0 +1,12 @@
+package cat_2;
+import java.util.*;
+public class swap_nibble {
+	public static void main(String[] args) {
+		Scanner x= new Scanner(System.in);
+		System.out.print("Enter the number:");
+		int n=x.nextInt();
+		System.out.println((n&240)>>4|(n&15)<<4);
+		System.out.println((n&0xf0)>>4|(n&0x0f)<<4);
+		
+	}
+}
