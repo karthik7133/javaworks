36035d716e490d1887175d928f753523a34b06c1
karthik <chkarthik853@gmail.com>
2025-09-04 12:04:54 +0530

    commit

---
+++ sts/src/cat_2/leaders.java
@@ -0,0 +1,21 @@
+package cat_2;
+import java.util.*;
+public class leaders {
+	public static void main(String [] ar){
+        //leaders in an array..
+        Scanner x=new Scanner(System.in);
+        int n=x.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=x.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=n-1;i>=0;i--){
+            if(a[i]>max){
+                max=a[i];
+                System.out.print(max+" ");
+            }
+        }
+    }
+}
+
