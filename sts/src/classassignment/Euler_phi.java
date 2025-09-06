f52cb9be9ec5d7b0328475463b4818421a73beba
karthik <chkarthik853@gmail.com>
2025-09-06 15:10:12 +0530

    commit

--- sts/src/classassignment/Euler_phi.java
+++ sts/src/classassignment/Euler_phi.java
@@ -2,16 +2,20 @@ package classassignment;
 
 public class Euler_phi {
 	public static void main(String[] args) {
-		int a=500;double temp=a;
+		int a=63;double temp=a;
 		for(int i=2;i*i<=a;i++) {
 			if(a%i==0) {
 				while(a%i==0) {
 					a/=i;
 				}temp=temp*(1.0-(1.0/i));
-			}
-		}if(a>2) 
+				
+			}System.out.println(i+" ");
+		}if(a>2) {
 			temp=temp*(1.0-(1.0/a));
+			System.out.println(a+" ");
 
+		}
+			
 		System.out.println((int)temp);
 	}
 }
