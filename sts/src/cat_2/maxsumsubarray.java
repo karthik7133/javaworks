6a447d55fda39ddf08405116d4c8799206a96507
karthik <chkarthik853@gmail.com>
2025-09-12 21:31:55 +0530

    commit

--- sts/src/cat_2/maxsumsubarray.java
+++ sts/src/cat_2/maxsumsubarray.java
@@ -1,8 +1,9 @@
 package cat_2;
 import java.util.*;
 public class maxsumsubarray {
+	static Scanner x= new Scanner(System.in);
 	public static void main(String[] args) {
-	     Scanner x= new Scanner(System.in);
+	     
 	     int n=x.nextInt();
 	     int a[]= new int [n];
 	     for(int i=0;i<n;i++)a[i]=x.nextInt();
@@ -15,4 +16,18 @@ public class maxsumsubarray {
 	         }
 	     }System.out.println(max);
 	 }
+	//bruteforce..
+	public void bruteforce() {
+		 int n=x.nextInt();
+	        int a[]= new int [n];
+	        for(int i=0;i<n;i++)a[i]=x.nextInt();
+	        int max=Integer.MIN_VALUE;
+	        for(int i=0;i<n;i++){
+	            int p=1;
+	            for(int j=i;j<n;j++){
+	                p=p*a[j];
+	                max=Math.max(p,max);
+	            }
+	        }System.out.println(max);
+	}
 }
