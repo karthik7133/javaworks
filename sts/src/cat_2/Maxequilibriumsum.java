1209728ac208549e06d9882b3f69ddb669b65eb5
karthik <chkarthik853@gmail.com>
2025-09-17 11:15:58 +0530

    commit

---
+++ sts/src/cat_2/Maxequilibriumsum.java
@@ -0,0 +1,26 @@
+package cat_2;
+//max equilibrium sum
+import java.util.*;
+public class Maxequilibriumsum {
+	public static void main(String[] args) {
+	     Scanner x = new Scanner(System.in);
+	     int n=x.nextInt();
+	     int a[] = new int[n];
+	     
+	     for(int i=0;i<n;i++)a[i]=x.nextInt();
+	     int psum=a[0];
+	     int ssum=a[n-1];
+	     int max=0;
+	     int i=0;int j=n-1;
+	     while(i<=j){
+	         if(psum==ssum)max=Math.max(max,psum);
+	         if(psum<ssum){
+	             i++;
+	             psum+=a[i];}
+	         else {j--;
+	             ssum+=a[j];}
+	     }System.out.println(max);
+	 }
+}
+
+
