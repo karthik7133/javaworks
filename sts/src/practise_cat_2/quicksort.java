ec77a2664de93bc20a17b34e855fabf7960026ee
karthik <chkarthik853@gmail.com>
2025-10-07 08:10:38 +0530

    commit

--- sts/src/practise_cat_2/quicksort.java
+++ sts/src/practise_cat_2/quicksort.java
@@ -3,34 +3,32 @@ import java.util.*;
 
 public class quicksort {
 	public static void main(String[] args) {
-		Scanner x =new Scanner(System.in);
+		Scanner x = new Scanner(System.in);
 		int n=x.nextInt();
-		int a[]=new int[n];
+		int a[] = new int[n];
 		for(int i=0;i<n;i++)a[i]=x.nextInt();
-		div(a,0,n-1);
+		divide(0,n-1,a);
 		System.out.println(Arrays.toString(a));
 	}
-
-	private static void div(int[] a, int l, int h) {
-		if(l<h) {
-			int pi=q(a,l,h);
-			div(a,l,pi-1);
-			div(a,pi+1,h);
-		}
-		
+	static void divide(int l,int r,int a[]) {
+		if(l>=r)return;
+		int pi=sort(l,r,a);
+		divide(l,pi-1,a);
+		divide(pi+1,r,a);
 	}
-	public static int q(int a[],int l,int h) {
-		int i=l-1,p=a[h];
-		for(int j=l;j<h;j++) {
+	static int sort(int l,int r,int a[]) {
+		int i=l-1;int p=a[r];
+		for(int j=l;j<r;j++) {
 			if(a[j]<p) {
-				i++;swap(a,i,j);
+				i++;swap(i,j,a);
 			}
-		}swap(a,i+1,h);
+		}swap(i+1,r,a);
 		return i+1;
 	}
-	public static void swap(int a[],int l,int h) {
-		int temp=a[l];
-		a[l]=a[h];
-		a[h]=temp;
+	static void swap(int a,int b,int ar[]) {
+		int t=ar[a];
+		ar[a]=ar[b];
+		ar[b]=t;
 	}
+	
 }
