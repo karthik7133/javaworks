5cb4c57b356a6da1dc879ea8bb70722239657b4e
karthik <chkarthik853@gmail.com>
2025-10-07 08:10:59 +0530

    commit

--- sts/src/practise_cat_2/maxprodsubarray.java
+++ sts/src/practise_cat_2/maxprodsubarray.java
@@ -10,8 +10,8 @@ public class maxprodsubarray {
 		for(int i=0;i<n;i++) {
 			int p=1;
 			for(int j=i;j<n;j++) {
-				 p*=a[j];max=Math.max(max, p);
-				
+				p*=a[j];
+				max =Math.max(max, p);
 			}
 		}System.out.println(max);
 	}
