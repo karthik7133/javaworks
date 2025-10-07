47384475499ff0d6e978426bcc82f75f29775eb7
karthik <chkarthik853@gmail.com>
2025-10-07 08:11:07 +0530

    commit

--- sts/src/practise_cat_2/max_eq_sum.java
+++ sts/src/practise_cat_2/max_eq_sum.java
@@ -2,22 +2,21 @@ package practise_cat_2;
 import java.util.*;
 public class max_eq_sum {
 	public static void main(String[] args) {
-		Scanner x = new Scanner(System.in);
-		System.out.println("Enter the array Length and array Elements:");
+		Scanner x = new Scanner (System.in);
 		int n=x.nextInt();
-		int a[] = new int[n];
-		for(int i=0;i<n;i++)a[i]=x.nextInt();
+		int a[] = new int[n];for(int i=0;i<n;i++)a[i]=x.nextInt();
 		int i=0,j=n-1;
-		int psum=a[i];
-		int ssum=a[j],max=Integer.MIN_VALUE;
+		int psum=a[i],ssum=a[j];
+		int max=Integer.MIN_VALUE;
 		while(i<=j) {
 			if(psum==ssum)max=Math.max(max, psum);
 			if(psum<ssum) {
 				i++;psum+=a[i];
-			}else {
-				j--;
-				ssum+=a[j];
 			}
-		}System.out.print(max);
+			else {
+				j--;ssum+=a[j];
+			}
+		}
+		System.out.println(max);
 	}
 }
