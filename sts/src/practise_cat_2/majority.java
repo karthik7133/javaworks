e7eb62274fce28b7643bf4eaab8ea127d18485f9
karthik <chkarthik853@gmail.com>
2025-10-07 08:11:17 +0530

    commit

--- sts/src/practise_cat_2/majority.java
+++ sts/src/practise_cat_2/majority.java
@@ -2,20 +2,23 @@ package practise_cat_2;
 import java.util.*;
 public class majority {
 	public static void main(String ar[]) {
-		Scanner x = new Scanner(System.in);
-		System.out.println("Enter the array size and input:");
+		Scanner x = new Scanner (System.in);
 		int n=x.nextInt();
-		int a[] = new int[n];
+		int a[]=new int[n];
 		for(int i=0;i<n;i++)a[i]=x.nextInt();
+		
 		int count=0,p=0;
 		for(int i=1;i<n;i++) {
-			int c=a[i];
-			if(count==0)p=a[i-1];
+			int c=a[i-1];
+			if(count==0) {p=a[i-1];count++;}
 			else if(p==c)count++;
 			else count--;
-		}count=0;
-		for(int i=0;i<n;i++)if(a[i]==p)count++;
+		}
+		count=0;
+		for(int i=0;i<n;i++) {
+			if(a[i]==p)count++;
+		}
 		if(count>n/2)System.out.println(p);
-		else System.out.println("No majority element");
+		else System.out.println("NO majority element!");
 	}
 }
