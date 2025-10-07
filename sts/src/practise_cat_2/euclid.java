5ddffb6372d4c6fe61515a54b4945e4122161ff3
karthik <chkarthik853@gmail.com>
2025-10-07 08:11:43 +0530

    commit

--- sts/src/practise_cat_2/euclid.java
+++ sts/src/practise_cat_2/euclid.java
@@ -3,12 +3,13 @@ import java.util.*;
 public class euclid {
 	public static void main(String[] args) {
 		Scanner x = new Scanner(System.in);
-		System.out.print("Enter two nums to find gcd:");
-		int a=x.nextInt(),b=x.nextInt();
-		System.out.println(gcd(a,b));
+		int n=x.nextInt();
+		int a=x.nextInt();
+		System.out.print(gcd(n,a));
 	}
-	static int gcd(int a,int b) {
+	public static int gcd(int a,int b) {
 		if(a==0)return b;
 		return gcd(b%a,a);
 	}
+	
 }
