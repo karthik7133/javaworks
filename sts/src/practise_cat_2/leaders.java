d5b0773d01301e99e6ab77af51149dc282e48449
karthik <chkarthik853@gmail.com>
2025-10-07 08:11:26 +0530

    commit

--- sts/src/practise_cat_2/leaders.java
+++ sts/src/practise_cat_2/leaders.java
@@ -3,15 +3,12 @@ import java.util.*;
 public class leaders {
 	public static void main(String[] args) {
 		Scanner x = new Scanner(System.in);
-		int n=x.nextInt();
-		int a[] = new int[n];
+		int n=x.nextInt();int a[] = new int[n];
 		for(int i=0;i<n;i++)a[i]=x.nextInt();
 		int max=Integer.MIN_VALUE;
 		for(int i=n-1;i>=0;i--) {
-			if(a[i]>max) {
-				max=a[i];
-				System.out.println(max);
-			}
+			if(a[i]>max) {max=Math.max(max, a[i]);
+			System.out.println(max);}
 		}
 	}
 }
