c916a214895ab92d69fb7d7e019adffd768d1241
karthik <chkarthik853@gmail.com>
2025-10-07 08:10:23 +0530

    commit

--- sts/src/practise_cat_2/selectionsort.java
+++ sts/src/practise_cat_2/selectionsort.java
@@ -2,18 +2,19 @@ package practise_cat_2;
 import java.util.*;
 public class selectionsort {
 	public static void main(String[] args) {
-		Scanner x = new Scanner (System.in);
+		Scanner x = new Scanner(System.in);
 		int n=x.nextInt();
 		int a[] = new int[n];
 		for(int i=0;i<n;i++)a[i]=x.nextInt();
 		for(int i=0;i<n;i++) {
-			int small=i;
+			int s=i;
 			for(int j=i+1;j<n;j++) {
-				if(a[small]>a[j])small=j;
+				if(a[s]>a[j])s=j;
 			}
 			int t=a[i];
-			a[i]=a[small];
-			a[small]=t;
-		}System.out.println(Arrays.toString(a));
+			a[i]=a[s];
+			a[s]=t;
+		}
+		System.out.println(Arrays.toString(a));
 	}
 }
