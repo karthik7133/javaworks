4c0e6fc57afd796f9b4a215dac5c18274947c25e
karthik <chkarthik853@gmail.com>
2025-10-22 14:50:18 +0530

    commit

--- practiseproblems/src/karthik/cla/maxflow.java
+++ practiseproblems/src/karthik/cla/maxflow.java
@@ -1,16 +1,15 @@
 package karthik.cla;
 import java.util.*;
 public class maxflow {
-	
-	static boolean bfs(int [] []c,int []p,int src ,int si,int n) {
-		Queue<Integer>q = new LinkedList<>();
-		boolean vi[]= new boolean[n];
-		q.add(src);
+	static boolean bfs(int [][]c,int p[],int src,int si,int n) {
+		Queue<Integer>q=new LinkedList<>();
+		boolean vi[]=new boolean[n];
 		vi[src]=true;
+		q.add(src);
 		while(!q.isEmpty()) {
 			int u=q.poll();
 			for(int v=0;v<n;v++) {
-				if(!vi[v]&& c[u][v]>0) {
+				if(!vi[v]&&c[u][v]>0) {
 					vi[v]=true;
 					q.add(v);
 					p[v]=u;
@@ -18,33 +17,31 @@ public class maxflow {
 				}
 			}
 		}
-		return false ;
+		return false;
+		
 	}
 	static int karp(int c[][],int src,int si,int n) {
-		int max=0;
 		int p[]= new int[n];
+		int max=0;
 		while(bfs(c,p,src,si,n)) {
-			int path=Integer.MAX_VALUE;
+			int pat=Integer.MIN_VALUE;
 			for(int v=si;v!=src;v=p[v]) {
 				int u=p[v];
-				path=Math.min(path, c[u][v]);
+				pat=Math.max(pat, c[u][v]);
 			}
 			for(int v=si;v!=src;v=p[v]) {
 				int u=p[v];
-				c[u][v]-=path;
-				c[v][u]+=path;
-			}max+=path;
+				c[u][v]-=pat;
+				c[v][u]+=pat;
+			}max+=pat;
 		}return max;
 	}
 	public static void main(String[] args) {
 		Scanner x = new Scanner(System.in);
-		
 		int n=x.nextInt();int m=x.nextInt();
-		int c[][]=new int[n][n];
+		int c[][]= new int[n][n];
 		for(int i=0;i<m;i++) {
-			int u=x.nextInt();
-			int v=x.nextInt();
-			int ca=x.nextInt();
+			int u=x.nextInt();int v=x.nextInt();int ca=x.nextInt();
 			c[u][v]+=ca;
 		}
 		int src=0,si=n-1;
