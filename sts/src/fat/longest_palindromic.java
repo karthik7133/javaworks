5828126906b1be071e003f8fe936962d817ceccd
karthik <chkarthik853@gmail.com>
2025-11-20 14:18:55 +0530

    commit

--- sts/src/fat/longest_palindromic.java
+++ sts/src/fat/longest_palindromic.java
@@ -33,3 +33,44 @@ public class longest_palindromic {
 		return a.substring(si,si+ml);
 	}
 }
+/*
+ * package daa;
+import java.util.*;
+
+public class lab_7 {
+    public static void main(String ar[]) {
+        Scanner x = new Scanner(System.in);
+        String a = x.next();
+        String result = longestPalindrome(a);
+        System.out.println("Length: " + result.length());
+        System.out.println("Substring: " + result);
+    }
+    
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);      // Odd length
+            int len2 = expandAroundCenter(s, i, i + 1);  // Even length
+            int len = Math.max(len1, len2);
+            
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        
+        return s.substring(start, end + 1);
+    }
+    
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
+ */
