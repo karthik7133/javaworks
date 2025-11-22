5f1306610f4e8ec5759a030a6e6e8ebc1f0fea78
karthik <chkarthik853@gmail.com>
2025-11-22 15:18:15 +0530

    commit

--- sts/src/fat/permutations.java
+++ sts/src/fat/permutations.java
@@ -1,34 +1,36 @@
 package fat;
+import java.util.HashSet;
 //no of permutations =>n!
 //t.c and s.c.
 //i/p string output=> no of permutations or what are permutations.
 public class permutations {
-
+	static HashSet<String > set= new HashSet<>();
     // Function to print all permutations
     public static void printPermutations(String str, String ans) {
         // Base case: if string is empty, print the answer
         if (str.length() == 0) {
-            System.out.println(ans);
+        	set.add(ans);
             return;
         }
-
         // Loop through the string
         for (int i = 0; i < str.length(); i++) {
             // Pick each character one by one
             char ch = str.charAt(i);
-
             // Remaining string after excluding the picked character
             String ros = str.substring(0, i) + str.substring(i + 1);
-
             // Recursive call
             printPermutations(ros, ans + ch);
         }
     }
 
     public static void main(String[] args) {
-        String s = "ABC";  // You can change this to any string
+        String s = "aabc";  // You can change this to any string
         System.out.println("All permutations of " + s + " are:");
         printPermutations(s, "");
+        System.out.println("No of permutation are :"+set.size());
+        for(String st :set) {
+        	System.out.println(st+" ");
+        }
     }
 }
 
