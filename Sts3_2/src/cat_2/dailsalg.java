b1d3e44447177efc0e564286f1be24e118bfa7ca
karthik <chkarthik853@gmail.com>
2026-03-19 10:51:45 +0530

    commit

--- Sts3_2/src/cat_2/dailsalg.java
+++ Sts3_2/src/cat_2/dailsalg.java
@@ -1,37 +1,91 @@
 package cat_2;
+
 import java.util.*;
+
 public class dailsalg {
-	public static void main(String[] args) {
-		
-	}
-	static void alg(List<List<int []>>adj,int src,int w,int v) {
-		int dis[] = new int[v];
-		for(int i:dis)dis[i]=Integer.MAX_VALUE;
-		List<ArrayDeque<Integer>>buckets = new ArrayList<>(w+1);
-		for(int i=0;i<=w;i++) {
-			buckets.add(new ArrayDeque<>());
-		}
-		buckets.get(0).add(src);
-		dis[src]=0;
-		int done=0;
-		int idx=0;int d=0;
-		while(done<v) {
-			while(buckets.get(idx).isEmpty()) {
-				idx=(idx+1)%(w+1);d++;
-			}
-			int cur=buckets.get(idx).poll();
-			if(dis[cur]<d)continue;
-			done++;
-			for(int [] edge:adj.get(cur)) {
-				int n=edge[0];
-				int wi=edge[1];
-				if(dis[cur]+w<dis[n]) {
-					dis[n]=dis[cur]+w;
-					buckets.get(dis[n]%(w+1)).add(n);
-				}
-				
-			}
-		}
-		
-	}
+
+    static void dial(List<List<int[]>> adj, int src, int W, int v) {
+
+        int[] dist = new int[v];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        dist[src] = 0;
+
+        List<ArrayDeque<Integer>> buckets = new ArrayList<>(W + 1);
+
+        for (int i = 0; i <= W; i++)
+            buckets.add(new ArrayDeque<>());
+
+        buckets.get(0).add(src);
+
+        int done = 0;
+        int idx = 0;
+        int d = 0;
+
+        while (done < v) {
+
+            while (buckets.get(idx).isEmpty()) {
+                idx = (idx + 1) % (W + 1);
+                d++;
+            }
+
+            int curr = buckets.get(idx).poll();
+
+            if (dist[curr] < d)
+                continue;
+
+            done++;
+
+            for (int[] edge : adj.get(curr)) {
+
+                int n = edge[0];
+                int w = edge[1];
+
+                if (dist[curr] + w < dist[n]) {
+
+                    dist[n] = dist[curr] + w;
+
+                    buckets.get(dist[n] % (W + 1)).add(n);
+                }
+            }
+        }
+
+        for (int i = 0; i < v; i++) {
+            System.out.println("Distance to " + i + " = " + dist[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int v = sc.nextInt(); // number of vertices
+        int e = sc.nextInt(); // number of edges
+
+        List<List<int[]>> adj = new ArrayList<>();
+
+        for (int i = 0; i < v; i++)
+            adj.add(new ArrayList<>());
+
+        int W = 0;
+
+        for (int i = 0; i < e; i++) {
+
+            int src = sc.nextInt();
+            int dest = sc.nextInt();
+            int w = sc.nextInt();
+
+            adj.get(src).add(new int[]{dest, w});
+            adj.get(dest).add(new int[]{src, w}); // undirected graph
+
+            if (w > W)
+                W = w;
+        }
+
+        int start = sc.nextInt();
+
+        dial(adj, start, W, v);
+
+        sc.close();
+    }
 }
